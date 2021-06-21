2620dd3bb5d26d97da0ffd802d650c406a63efbb
VaagnDavtyan <78389280+VaagnDavtyan@users.noreply.github.com>
2021-06-21 16:43:11 -0700

    Initial commit

---
+++ src/componets/Team.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./team.css";
+
+const Team = ({ source, teamName, count, func, className }) => {
+  return (
+    <div className={`wrapper ${className}`}>
+      <img src={source} alt="team is not found" />
+      <h3>{teamName}</h3>
+      <button onClick={() => func(teamName)}>Vote</button>
+      <p>Vote count: {count}</p>
+    </div>
+  );
+};
+
+export default Team;
